72c48344c09eb41c33cf072e404037eda77be723
@wwwjim <send@jimmy.email>
2019-12-17 16:40:23 -0800

    documentation: adds heading links to all headings

--- components/Text.js
+++ components/Text.js
@@ -1,9 +1,20 @@
 import * as React from 'react';
 import * as Constants from '~/common/constants';
+import * as Strings from '~/common/strings';
 
 import { css } from 'react-emotion';
 
+let iterator = 0;
+
 const MAX_WIDTH = 768;
+
+const STYLES_ID_ANCHOR = css`
+  position: absolute;
+  top: -112px;
+  left: 0;
+  width: 1px;
+  height: 1px;
+`;
 
 const STYLES_HEADING = css`
   font-family: 'medium';
@@ -16,10 +27,39 @@ const STYLES_HEADING = css`
   width: 100%;
   padding: 0 24px 0 24px;
   margin: 0 auto 0 auto;
+  display: block;
+  cursor: pointer;
+  color: ${Constants.theme.pageText};
+  text-decoration: none;
+  transition: 200ms ease color;
+  position: relative;
+
+  :visited {
+    color: ${Constants.theme.pageText};
+  }
+
+  :hover {
+    color: ${Constants.theme.action};
+  }
 `;
 
 export const H1 = props => {
-  return <h1 {...props} className={STYLES_HEADING} />;
+  const candidate =
+    props.children && props.children[0].props.block
+      ? props.children[0].props.block.text
+      : null;
+  if (!candidate) {
+    return <h1 {...props} className={STYLES_HEADING} />;
+  }
+
+  const id = `${Strings.createSlug(candidate)}-${iterator}`;
+  iterator = iterator + 1;
+  return (
+    <a {...props} href={`#${id}`} className={STYLES_HEADING}>
+      <span className={STYLES_ID_ANCHOR} id={id} />
+      {props.children}
+    </a>
+  );
 };
 
 const STYLES_HEADING_TWO = css`
@@ -33,10 +73,39 @@ const STYLES_HEADING_TWO = css`
   width: 100%;
   padding: 0 24px 0 24px;
   margin: 48px auto 0 auto;
+  display: block;
+  cursor: pointer;
+  color: ${Constants.theme.pageText};
+  text-decoration: none;
+  transition: 200ms ease color;
+  position: relative;
+
+  :visited {
+    color: ${Constants.theme.pageText};
+  }
+
+  :hover {
+    color: ${Constants.theme.action};
+  }
 `;
 
 export const H2 = props => {
-  return <h2 {...props} className={STYLES_HEADING_TWO} />;
+  const candidate =
+    props.children && props.children[0].props.block
+      ? props.children[0].props.block.text
+      : null;
+  if (!candidate) {
+    return <h2 {...props} className={STYLES_HEADING_TWO} />;
+  }
+
+  const id = `${Strings.createSlug(candidate)}-${iterator}`;
+  iterator = iterator + 1;
+  return (
+    <a {...props} href={`#${id}`} className={STYLES_HEADING_TWO}>
+      <span className={STYLES_ID_ANCHOR} id={id} />
+      {props.children}
+    </a>
+  );
 };
 
 const STYLES_HEADING_THREE = css`
@@ -50,10 +119,39 @@ const STYLES_HEADING_THREE = css`
   width: 100%;
   padding: 0 24px 0 24px;
   margin: 16px auto 0 auto;
+  display: block;
+  cursor: pointer;
+  color: ${Constants.theme.pageText};
+  text-decoration: none;
+  transition: 200ms ease color;
+  position: relative;
+
+  :visited {
+    color: ${Constants.theme.pageText};
+  }
+
+  :hover {
+    color: ${Constants.theme.action};
+  }
 `;
 
 export const H3 = props => {
-  return <h3 {...props} className={STYLES_HEADING_THREE} />;
+  const candidate =
+    props.children && props.children[0].props.block
+      ? props.children[0].props.block.text
+      : null;
+  if (!candidate) {
+    return <h3 {...props} className={STYLES_HEADING_THREE} />;
+  }
+
+  const id = `${Strings.createSlug(candidate)}-${iterator}`;
+  iterator = iterator + 1;
+  return (
+    <a {...props} href={`#${id}`} className={STYLES_HEADING_THREE}>
+      <span className={STYLES_ID_ANCHOR} id={id} />
+      {props.children}
+    </a>
+  );
 };
 
 const STYLES_PARAGRAPH = css`
@@ -148,4 +246,3 @@ export const BLOCKQUOTE = props => {
     </span>
   );
 };
-
