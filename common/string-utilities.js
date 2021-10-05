025dc6eb8462f05c554da1031d7350c4b911d89a
johnnymatthews <johnnymatthews@protonmail.com>
2021-10-05 17:09:01 -0400

    Fresh start.

--- common/string-utilities.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as Strings from '~/common/strings';
-
-// NOTE(jim)
-// https://github.com/EfogDev/react-process-string/blob/master/index.js
-export const insertMentionsIntoString = (text, options = [], Component) => {
-  return Strings.clientInsertElements([...options])(text);
-};
--- index.js
+++
@@ -1,6 +0,0 @@
-require('@babel/register')({
-  presets: ['@babel/preset-env'],
-  ignore: ['node_modules', '.next'],
-});
-
-module.exports = require('./server.js');
